8b56b54bcb63f2a14f5f643c513cd0b7e1d30126
Ruchika Shashidhara <48530409+RuchikaShashidhara@users.noreply.github.com>
2020-07-06 20:43:50 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,30 +11,35 @@ void run_test(std::vector<int>, std::vector<int>, int);
 int main()
 {
     // Test case 1:
-    std::vector<int> o1 =  {1, 1, 1, 2, 3, 1, 1, 2, 3, 1};
-    std::vector<int> a1 =  {5, 7, 2,    2, 3, 8,    4, 8};
+    // Atleast 2 threads perform different functions - insertion, deletion, search considering all edge cases
+    std::vector<int> o1 =  {1, 1, 1, 2, 3, 1, 1, 2, 3, 1, 1, 3, 2, 1, 3, 1, 1,  3, 2, 3, 1, 3, 1, 2, 1};
+    std::vector<int> a1 =  {5, 7, 2,    2, 3, 8,    4, 8, 8, 8,    9, 1, 1, 3, 10,    8, 1, 8, 4,    7};
     std::cout << "Running test case 1...\n";
-    run_test(o1, a1, 10);
+    run_test(o1, a1, 25);
 
     // Test case 2:
-    std::vector<int> o2 =  {2, 2, 3, 2, 3};
-    std::vector<int> a2 =  {      1,    2};
+    // Deletion and search when no elements are present
+    std::vector<int> o2 =  {2, 2, 3, 2, 3, 2, 2, 3, 3, 2};
+    std::vector<int> a2 =  {      1,    2,       1, 2};
     std::cout << "Running test case 2...\n";
-    run_test(o2, a2, 5);
+    run_test(o2, a2, 10);
 
     // Test case 3:
+    // Atleast 2 threads perform different functions - insertion, deletion, search
     std::vector<int> o3 =  {2, 3, 1, 1, 2, 3, 1, 1, 2, 3, 1, 1,  1,  3, 2, 2, 2, 1,  1,  1, 3, 3, 1, 2, 1};
     std::vector<int> a3 =  {   7, 7, 2,    2, 3, 8,    4, 8, 1, 17, 11,          4, 12, 11, 4, 9, 5,    6};
     std::cout << "Running test case 3...\n";
     run_test(o3, a3, 25);
 
     // Test case 4:
+    // Atleast 2 threads perform different functions - insertion, deletion, search
     std::vector<int> o4 =  {2, 2, 3, 2, 3, 1, 1, 2, 3, 1, 1,  1, 3, 3, 1, 1, 2, 3, 1, 1, 2, 2, 2, 2,  3};
     std::vector<int> a4 =  {      1,    2, 1, 2,    7, 8, 9, 10, 2, 4, 5, 7,    7, 3, 1,             10};
     std::cout << "Running test case 4...\n";
     run_test(o4, a4, 25);
 
     // Test case 5:
+    // Insertion of elements in decreasing order, with a few deletions and serach
     std::vector<int> o5 =  { 1, 1,  1, 2,  3,  1, 1, 2, 3, 1, 1,  3, 1, 1, 2};
     std::vector<int> a5 =  {17, 12, 11,    12, 9, 8,    8, 6, 5,  4, 4, 3};
     std::cout << "Running test case 5...\n";
